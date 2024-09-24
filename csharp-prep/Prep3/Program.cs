93a50bffb9e77167b648aba688bbdce643c08617
lasobieralski <dreamer42day@gmail.com>
2024-09-23 20:07:04 -0600

    started prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,10 +1,31 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello Prep3 World!");
-        //test
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        if (magicNumber < guess)
+        {
+            Console.WriteLine("lower");
+            
+        }
+
+        else if (magicNumber > guess)
+        {
+            Console.WriteLine("higher");
+        }
+
+        else
+        {
+            Console.WriteLine("Congratulations you guessed correctly!");
+        }
     }
 }
