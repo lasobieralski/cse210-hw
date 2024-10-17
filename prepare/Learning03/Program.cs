1c4cc7da83c4c290b74774aa64ef9432dc736592
lasobieralski <dreamer42day@gmail.com>
2024-10-16 19:34:56 -0600

    created fraction code with encapsulation

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(5);
+        Fraction fraction3 = new Fraction(3,4);
+        Fraction fraction4 = new Fraction(1,3);
+
+        Console.WriteLine("Fraction: " + fraction1.GetFraction());
+        Console.WriteLine("Decimal: " + fraction1.GetDecimal());
+        Console.WriteLine("Fraction: " + fraction2.GetFraction());
+        Console.WriteLine("Decimal: " + fraction2.GetDecimal());
+        Console.WriteLine("Fraction: " + fraction3.GetFraction());
+        Console.WriteLine("Decimal: " + fraction3.GetDecimal());
+        Console.WriteLine("Fraction: " + fraction4.GetFraction());
+        Console.WriteLine("Decimal: " + fraction4.GetDecimal());
     }
 }
+
